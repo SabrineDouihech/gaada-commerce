44012e96bbaa8bb63f9caf8733e7eb172c02eb2b
jess <jasservaldi@gmail.com>
2021-07-31 16:13:04 +0100

    jess

--- gaada-project/react-client/src/components/App.jsx
+++ gaada-project/react-client/src/components/App.jsx
@@ -7,7 +7,6 @@ import Field from "./Field.jsx";
 import Basket from "./Basket.jsx";
 import axios from "axios";
 
-
 export default class App extends React.Component {
   
   constructor(props) {
@@ -18,18 +17,10 @@ export default class App extends React.Component {
       view: "home",
       user:{},
       basket: [],
-      serachType:"",
       };
     this.changeView = this.changeView.bind(this);
     this.getItems = this.getItems.bind(this);
     this.getUsers = this.getUsers.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    
-  }
-
-
-  handleChange(e){
-    this.setState({searchType: e.target.value})
   }
 
   componentDidMount() {
@@ -46,7 +37,6 @@ export default class App extends React.Component {
     })
     
   }
-
   getItems() {
     axios.get("/items").then((res) => {
       this.setState({ items: res.data });
@@ -63,23 +53,13 @@ export default class App extends React.Component {
       });
   }
 
-// filtered(){
-  
-// }
-  
-
   renderView() {
-
-    const filteredItems = this.state.items.filter((item)=>{
-      return item.itemType.includes(this.state.searchType);
-    })
-//console.log(filteredItems)
     const { view, items, users, basket, user } = this.state;
 
     if (view === "home") {
-      return <Home filteredItems={filteredItems} basket={basket} changeView={this.changeView} />;
+      return <Home items={items} basket={basket} changeView={this.changeView} />;
     } else if (view === "login") {
-      return <Login users={users} changeView={()=>{this.changeView('profile')}} />;
+      return <Login users={users} handleChange={()=>{this.changeView('profile')}} />;
     } else if (view === "sign up") {
       return <Signup handleChange={()=>{this.changeView('field')}}  />;
     } else if (view === "field"){
@@ -101,10 +81,10 @@ export default class App extends React.Component {
           className="logo"
           style={{cursor:"pointer"}}
           onClick={() => this.changeView("home")}> Ga3da commerce </span>
-            <span>
-              <input type="text" placeholder="Search..." onChange={this.handleChange} value={this.state.searchType}/>
-              
-            </span>
+            {/* <span>
+              <input type="text" />
+              <button>search</button>
+            </span> */}
           <span
             className={
               this.state.view === "home" ? "nav-selected" : "nav-unselected"
